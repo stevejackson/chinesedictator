a68df7d7966c0307dc5eb3e1273f9827e8f0552f
Steve Jackson <steven.j.jackson@gmail.com>
2011-11-23 18:39:39 -0500

    Initial locale test.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale]
+  end
+  
+  def url_options
+    { :locale => I18n.locale }.merge(super)
+  end
 end
