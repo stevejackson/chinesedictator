b64775c8c0124e706c8dfedf0e06154d824c7b44
Steve Jackson <steven.j.jackson@gmail.com>
2013-01-11 01:15:52 +0900

    remove secret token from repo.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ChineseDictator::Application.config.secret_token = 'ecbdadfdf42587dfsjo42papi31aacbce9a71cc7d9d82ac1612eff54e16a83d6a115dd78775ccca6e11e26626c2bbdb2b38b70b9bc6a7dc2cfe47201dfbcf15cff4fed5944ccddfd9139'
